e9802570ce58e85722d8667c9f81c417f34edf6f
Abhishek Mondal <70214469+Abhishek-e@users.noreply.github.com>
2026-07-20 02:43:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -274,7 +274,7 @@ app.get('/api/health', (req, res) => {
 
 // Serve the main HTML page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // Error handling middleware
